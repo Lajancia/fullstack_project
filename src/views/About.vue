f11f924f94e3321b550305ec912ea93a52e9a3e9
Lajancia <kie6974@hufs.ac.kr>
2021-12-10 17:52:38 +0900

    test2

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <div>
       <div class="absolute inset-y-0 right-0  h-full w-4/5">
         <div class="text-white">
-          updated-2021-12-10 this is the result of test change accur
+          last test to be done. yeeehhhh
         </div>
       </div>
     </div>
