daf82736bed5f7caeff391bc7d9189c22266f020
Lajancia <kie6974@hufs.ac.kr>
2021-12-10 16:07:25 +0900

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <div>
       <div class="absolute inset-y-0 right-0  h-full w-4/5">
-        updated-2021-12-09
+        updated-2021-12-10
       </div>
     </div>
   </div>
