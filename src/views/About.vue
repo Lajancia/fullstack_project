93ef5a920464afe68f860bdbd8cf06a1cef71d95
Lajancia <kie6974@hufs.ac.kr>
2021-12-10 17:17:19 +0900

    엑세스 확인

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,9 @@
   <div class="about">
     <div>
       <div class="absolute inset-y-0 right-0  h-full w-4/5">
-        updated-2021-12-10 this is the result of test
+        <div class="text-white">
+          updated-2021-12-10 this is the result of test
+        </div>
       </div>
     </div>
   </div>
