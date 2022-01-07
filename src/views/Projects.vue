18b8da138c3bc679f9750dc94e2d467dedde471a
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-01-07 17:55:08 +0900

    test change

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="text-white">Sorry. This page is not ready</div>
+  <div class="text-white">Sorry. This page is not ready
+    <div>this will be changed soon</div>
+  </div>
 </template>
