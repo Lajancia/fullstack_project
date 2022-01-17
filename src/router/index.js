753b42a23da8661cea60b532e87e61e198ff89e9
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-01-17 18:06:00 +0900

    url fix

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Projects.vue')
   },
+    {
+    path: '/todolist',
+    name: 'Todolist',
+    meta: {
+        title: "Todo",
+      },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Todolist.vue')
+  },
 
 ]
 
