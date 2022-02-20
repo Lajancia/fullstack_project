36cc6920fa8aa6c512aa83ed5fa282d6d6e13ad6
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-02-20 22:51:33 +0900

    my page add

--- src/views/Achievement.vue
+++ src/views/Achievement.vue
@@ -2,14 +2,13 @@
   <body class="absolute bottom-0 right-0 h-full ">
     <div class="my-20 mr-10">
       <div class="text-white">
-        <div>
-          <h1>Achievement</h1>
-          <ul>
-            <li v-for="todo in todos" :key="todo.id" class="achieve">
-              <span>{{ todo.date }}</span>
-            </li>
-          </ul>
+        <div v-if="user">
+          <h1>My Page</h1>
+          <h1 class="text-yellow-500 text-6xl py-2">User Information</h1>
+          <p class="text-white text-4xl py-2">ID : {{ user.email }}</p>
+          <p class="text-white text-4xl py-2">Name : {{ user.nick }}</p>
         </div>
+        <div v-else>Login Needed</div>
       </div>
     </div>
   </body>
