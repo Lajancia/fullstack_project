2f615c64cd9b010b156f183f005cb6303392196e
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-05-06 13:19:17 +0200

    vps test

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,12 @@ module.exports = {
         }
     }
 },
-  devServer: { 
+  devServer: {
+    disableHostCheck: true,
+        port: 8081,
+        public: '0.0.0.0:8081',
     proxy: { 
+      
       '/api': { 
         target: 'http://localhost:3000/api',
         changeOrigin: true, 
